a1ec2d46348004027fb27a3cb3ec5f798f17a1e5
g-battaglia <battaglia.giacomo@icloud.com>
2022-09-16 23:09:02 +0200

    Fix

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
+import typer
+
 from os import walk
 from mutagen.flac import FLAC
 from pathlib import Path
 from tqdm import tqdm
 
-import typer
 
 # Local
 from utils import folder_count
 
-TIDAL_DIR = "tmp"
+TIDAL_DIR = "/Volumes/SD1T/MUSIC/TIDAL"
 
 app = typer.Typer()
 
 
 @app.command()
-def main(music_folder: str):
-    TIDAL_DIR = music_folder
-    if not music_folder:
-        return
+def main():
+    main_dir = Path(TIDAL_DIR)
 
     for root, dirs, files in tqdm(walk(TIDAL_DIR), total=(folder_count(TIDAL_DIR) + 1)):
         for file in files:
@@ -26,19 +25,23 @@ def main(music_folder: str):
 
             if file.endswith(".flac"):
                 path = Path(root, file)
-
-                album_name = path.parents[-3].name.strip()
-                if album_name == "Various Artists":
-                    continue
-
-                artist_name = path.parents[-4].name.strip()
                 audio = FLAC(path)
+
+                index = path.parents.index(main_dir)
+
+                album_name = path.parents[index - 1].name.strip()
+
+                audio["album"] = album_name
 
                 if audio.get("artist"):
                     audio["albumartist"] = audio["artist"]
 
+                artist_name = path.parents[index - 1].name.strip()
+                if artist_name == "Various Artists":
+                    audio["artist"] = audio["album"]
+                    continue
+
                 audio["artist"] = artist_name
-                audio["album"] = album_name
 
                 audio.save()
 
