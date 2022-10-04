d332f8241d31d4f564c584ba3923e052aeede813
g-battaglia <battaglia.giacomo@icloud.com>
2022-10-05 00:24:16 +0200

    Required python version updated

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setup(
     name="PyMusicOrganizer",
-    version="0.0.3",
+    version="0.0.4",
     author="Giacomo Battaglia",
     author_email="battaglia.giacomo@yahoo.it",
     description="CLI tool to keep your music organized.",
